980ab286fefd1012a5d7c613672a58bf1c9ab64b
riyanlasso <riyanlasso@gmail.com>
2021-02-24 03:13:54 +0700

    Praktikum 2

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about() { 
+    echo 'NIM : 1941720203<br>Nama : Fitranza Akbar Sarrazin';
+    }
+}
